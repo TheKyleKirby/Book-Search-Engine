20881b15de816b5f2d19caba31e72d0f8b4fea47
Reputation <kylekirby86@gmail.com>
2024-08-23 12:25:27 -0600

    Archived folders pushed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 module.exports = mongoose.connection;
