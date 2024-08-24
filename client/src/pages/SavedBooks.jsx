b7475928d5e51951669f22b8ef30aa10062cfb9c
Reputation <kylekirby86@gmail.com>
2024-08-24 13:58:23 -0600

    Remove errors

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { Container, Card, Button, Row, Col } from "react-bootstrap";
 
 // Import Apollo Client hooks and custom utilities.
